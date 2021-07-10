55895c5d8c1eec01aebdb4cd8b443849d545fc6d
angiecruz <angiecruzalmanza@gmail.com>
2021-07-10 00:31:37 -0500

    data imported

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,9 @@ var svg = d3
 
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  // 3.- Import data 
+
+  d3.csv("assets/data/data.csv").then(function(data) {
+    console.log(data);
+  });
